0af7aa2d2ab1f878e1429febb728a2600dda0940
asmyasnikova83 <asmyasnikova83@gmail.com>
2026-07-16 16:28:41 +0300

    rm  from main dir

--- Impedance_table_with_caution.py
+++ Impedance_table_with_caution.py
@@ -8,15 +8,20 @@ import pandas as pd
 # Этапы оценки
 этапы = ['При установке гарнитуры', 'После ношения гарнитуры в течение 20 минут']
 
-# Типы гарнитур
+"""
 гарнитуры = [
-    'Мокрые: NeoRec cap 21 PROFESSIONAL',
-    'Сухие: AnaRec cap 21 mini DRY active (новые)',
-    'Сухие: AnaRec cap 21 mini DRY active (старые)',
-    'NeoRec cap 21 mini DRY passive (новые)',
-    'NeoRec cap 21 mini DRY passive (старые)'
+    'Мокрые',
+    'DRY active + MCScap-DrA1 + Brush Flex',
+    'DRY active + MCScap-DrA1 + Brush Medium',
+    'DRY + MCScap-DrP1 + Brush Flex',
+    'DRY + MCScap-DrP1 + Brush Medium'
 ]
-
+"""
+гарнитуры = [
+    'Мокрые',
+    'DRY + MCScap-DrP1 + Brush Flex',
+    'DRY + MCScap-DrP1 + Brush Medium'
+]
 # Создаём данные
 data = []
 for испытуемый in испытуемые:
@@ -24,9 +29,10 @@ for испытуемый in испытуемые:
         for гарнитура in гарнитуры:
             data.append({
                 'Испытуемый': испытуемый,
-                'Этап': этап,
+                #'Этап': этап,
                 'Тип гарнитуры': гарнитура,
-                'Рейтинг уровня комфорта (1–7)': ''
+                #'Рейтинг уровня комфорта (1–7)': ''
+                'Импеданс': ''
             })
 
 # Создаём DataFrame
@@ -34,7 +40,8 @@ df = pd.DataFrame(data)
 
 # Сохраняем в Excel
 output_pics_dir = r"M:\DB Temp\physionet.org\files\dry_electrodes\epochs\tables"
-output_file = 'рейтинг_уровня_комфорта.xlsx'
+#output_file = 'рейтинг_уровня_комфорта.xlsx'
+output_file = 'Импендансы.xlsx'
 filepath = os.path.join(output_pics_dir, output_file)
 df.to_excel(filepath , index=False)
 
