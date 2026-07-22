804d53e21588dd6268506c25c155fffc3b41da4a
asmyasnikova83 <asmyasnikova83@gmail.com>
2026-07-22 12:19:56 +0300

    refactor

--- compute_plot_artifacts_share.py
+++ compute_plot_artifacts_share.py
@@ -10,9 +10,12 @@ headsets_mapping = cfg.headsets_mapping
 results = read_artifacts(subjects, headsets_base, recordings_base)
 df = artifacts_share_into_df(results)
 ############################################# Plot #############################################################
-plot_share_artifacts(df, aggtype='Recording', mapping=recordings_mapping, base=recordings_base, rest=0)
-plot_share_artifacts(df, aggtype='Headset',  mapping=headsets_mapping, base=headsets_base, rest=0)
+if recordings_base[0] == 'cycling':
+    plot_share_artifacts(df, aggtype='Headset', mapping=headsets_mapping, base=headsets_base, rest=2)
 
-combined_rest = combine_rest_data(results)
-df_rest = artifacts_share_into_df(combined_rest)
-plot_share_artifacts(df_rest, aggtype='Headset', mapping=headsets_mapping, base=headsets_base, rest=1)
+else:
+    plot_share_artifacts(df, aggtype='Recording', mapping=recordings_mapping, base=recordings_base, rest=0)
+    plot_share_artifacts(df, aggtype='Headset',  mapping=headsets_mapping, base=headsets_base, rest=0)
+    combined_rest = combine_rest_data(results)
+    df_rest = artifacts_share_into_df(combined_rest)
+    plot_share_artifacts(df_rest, aggtype='Headset', mapping=headsets_mapping, base=headsets_base, rest=1)
